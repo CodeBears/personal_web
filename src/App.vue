df01314fd5d1979a69ad6e59acc4fe1351c11d06
Johnny <qszwax0935@gmail.com>
2021-03-01 17:09:13 +0800

    add route

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/contact">Contact</router-link>
+  </div>
+  <router-view />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue';
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+#nav
+  padding: 30px
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+#nav a
+  font-weight: bold
+  color: #2c3e50
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+#nav a.router-link-exact-active
+  color: #42b983
+
+h2
+  color: rgb(145, 45, 45)
 </style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="contact">
+    <h1>This is an contact page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Contact',
+};
+</script>
