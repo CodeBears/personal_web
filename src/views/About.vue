6175aa74dc8688cff0e51d45a90ec6c6cad8c181
Johnny <qszwax0935@gmail.com>
2021-03-20 22:50:56 +0800

    edit title

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="commonContainer">
     <div class="about">
-      <h3>Backend Software Engineer in Jin Guan(10/2020 - present)</h3>
+      <h3>Backend Software Engineer in Golden Crown Technology Inc.(10/2020 - present)</h3>
       <ul>
         <li>Develope and maintenance the foreign exchange trading websites</li>
         <li>Web RESTful API</li>
@@ -9,7 +9,7 @@
         <li>Docker</li>
         <li>Testing</li>
       </ul>
-      <h3>Frontend Software Engineer in Shinewave(1/2020 - 6/2020)</h3>
+      <h3>Frontend Software Engineer in Shinewave Inc.(1/2020 - 6/2020)</h3>
       <ul>
         <li>Design project management web and work overtime application web in angular</li>
         <li>Typescript</li>
